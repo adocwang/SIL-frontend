5ab54075def509b1252af187d0c942c73f522e49
cescwang <cescwang@cescwangdeMacBook-Pro.local>
2017-02-13 00:49:42 +0800

    设置刷新时间

--- app/modules/sysconfig/module.js
+++ app/modules/sysconfig/module.js
@@ -1,12 +1,12 @@
 import angular from 'angular';
 import bootstrap from 'bootstrap';
 import config  from './config';
-import service from './service';
+//import service from './service';
 import TimeView from './time/';
 
 export default angular.module('app.modules.sysconfig', [])
 // .filter('filterType', filterType)
-.service('sysconfigService', service)
+//.service('sysconfigService', service)
 
 .component('timeView', TimeView)
 .config(config);
